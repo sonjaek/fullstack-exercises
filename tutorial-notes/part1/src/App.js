07f1d4c99fed7692a8215d516f8aa999c0f431ee
Sonja Ek <sonja3k@icloud.com>
2021-03-17 17:55:51 +0200

    Part 1b, Page re-rendering

--- tutorial-notes/part1/src/App.js
+++ tutorial-notes/part1/src/App.js
@@ -1,57 +1,10 @@
 import React from 'react'
 
-// React component names must be capitalized:
-const Hello = (props) => {
-  const { name, age } = props
-  // The logic for guessing the birth year is separated into its ofn function that is
-  // called when the component is rendered:
-  const bornYear = () => new Date().getFullYear() - age 
-  // bornYear() is actually a function inside function, which is normal in JavaScript.
-
+const App = (props) => {
+  const {counter} = props
   return (
-    <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>
-        So you were probably born in {bornYear()}
-      </p>
-    </div>
+    <div>{counter}</div>
   )
 }
 
-const App = () => {
-  const name = "Sonja"
-  const age = 23
-
-  return (
-    <>
-      <h1>Greetings</h1>
-      <Hello name="Mimosa" age={3 + 1} />
-      <Hello name={name} age={age} />
-    </>
-  );
-}
-
-export default App;
-
-// Notes:
-
-  // The code inside our functions is JSX, a syntax extension to JavaScript.
-  // JSX is recommended to be used with React to describe what the UI should look like.
-
-  // In practice, JSX is much like HTML with the distinction that with JSX you can 
-  // easily embed dynamic content by writing appropriate JavaScript within curly
-  // braces. The idea of JSX is quite similar to many templating languages, such as
-  // Thymeleaf used along with Java Spring, which are used on servers.
-
-  // In JSX, every tag needs to be closed: <br> doesn't work for newline, <br /> does.
-
-  // The idea of a root component called App, at the top of the component tree of the
-  // application, is a strong convention in React.
-
-  // The content of a React component usually needs to contain one root element, which
-  // in the App component is the div-element.
-    // The root element is stipulated, so <div> tags in the DOM tree can be made empty: <>
-    // An array of components is also a valid solution, but not wise or pretty.
-  
+export default App
--- tutorial-notes/part1/src/index.js
+++ tutorial-notes/part1/src/index.js
@@ -1,14 +1,14 @@
-import ReactDOM from 'react-dom';
-import App from './App';
+import ReactDOM from 'react-dom'
+import App from './App'
 
-// The following renders App.js contents into the div-element defined in 
-// public/index.html, having the id value 'root':
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
+let counter = 1
 
-// By default, the file public/index.html doesn't contain any HTML markup 
-// that is visible to us in the browser. You can try adding some HTML into
-// the file. However, when using React, all content that needs to be
-// rendered is usually defined as React components.
+const refresh = () => {
+  ReactDOM.render(<App counter={counter} />, 
+    document.getElementById('root'))
+}
+
+setInterval(() => {
+  refresh()
+  counter += 1
+}, 1000)
