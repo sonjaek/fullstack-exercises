92e08d815c8158912f805dfd452d22430ca855d0
Sonja Ek <sonja3k@icloud.com>
2021-03-21 15:23:31 +0200

    Part 1d, Conditional rendering

--- tutorial-notes/part1/src/App.js
+++ tutorial-notes/part1/src/App.js
@@ -1,30 +1,51 @@
 import React, { useState } from 'react' // useState is a function
 
-const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L')) // concat() makes a new array
-    // Do NOT use push() because it's a mutating method -> not good for React
-    setLeft(left + 1)
+  const History = (props) => {
+    if (props.allClicks.length === 0) {
+      return (
+        <div>
+          the app is used by pressing the buttons
+        </div>
+      )
+    }
+    return (
+      <div>
+        button press history: {props.allClicks.join(' ')}
+      </div>
+    )
   }
 
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
-  }
-
-  return (
-    <div>
-      {left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
-      {right}
-      <p>{allClicks.join(' ')}</p>
-    </div>
+  const Button = ({ handleClick, text }) => (
+    <button onClick={handleClick}>
+      {text}
+    </button>
   )
-}
+  
+  const App = () => {
+    const [left, setLeft] = useState(0)
+    const [right, setRight] = useState(0)
+    const [allClicks, setAll] = useState([])
+  
+    const handleLeftClick = () => {
+      setAll(allClicks.concat('L')) // concat() makes a new array
+      // Do NOT use push() because it's a mutating method -> not good for React
+      setLeft(left + 1)
+    }
+  
+    const handleRightClick = () => {
+      setAll(allClicks.concat('R'))
+      setRight(right + 1)
+    }
+  
+    return (
+      <div>
+        {left}
+        <Button handleClick={handleLeftClick} text='left' />
+        <Button handleClick={handleRightClick} text='right' />
+        {right}
+        <History allClicks={allClicks} />
+      </div>
+    )
+  }
 
 export default App
