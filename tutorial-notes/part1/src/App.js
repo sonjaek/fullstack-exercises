74926ae99729365f55a85c4a4cedc536f0261237
Sonja Ek <sonja3k@icloud.com>
2021-03-21 13:42:41 +0200

    Part 1c, Passing state to child components

--- tutorial-notes/part1/src/App.js
+++ tutorial-notes/part1/src/App.js
@@ -3,23 +3,31 @@ import React, { useState } from 'react' // useState is a function
 const App = () => {
   const [ counter, setCounter ] = useState(0)
 
-  // Event handler must be a function. If you write
-  // <button onClick={setCounter(counter + 1)}>, it's a *function call*
-  // and will not work at all.
-
   const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
   const setToZero = () => setCounter(0)
 
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={increaseByOne}>
-        plus
-      </button>
-      <button onClick={setToZero}>
-        zero
-      </button>
+      <Display counter={counter}/>
+      <Button handleClick={increaseByOne} text='plus'/>
+      <Button handleClick={setToZero} text='zero'/>
+      <Button handleClick={decreaseByOne} text='minus'/>
     </div>
+  )
+}
+
+const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
   )
 }
 
