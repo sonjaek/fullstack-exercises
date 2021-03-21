fcef30394bdb3eff6074ab0b89f08bb79b3b886a
Sonja Ek <sonja3k@icloud.com>
2021-03-21 15:16:36 +0200

    Part 1d, Handling arrays

--- tutorial-notes/part1/src/App.js
+++ tutorial-notes/part1/src/App.js
@@ -1,36 +1,30 @@
 import React, { useState } from 'react' // useState is a function
 
 const App = () => {
-  const [clicks, setClicks] = useState({
-    left: 0, right: 0
-  })
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
 
-  const handleLeftClick = () =>
-    setClicks({ ...clicks, left: clicks.left + 1 })
-
-  const handleRightClick = () =>
-    setClicks({ ...clicks, right: clicks.right + 1 })
-
-  /*
-  It's forbidden in React to mutate state directly. You can't do this:
   const handleLeftClick = () => {
-    clicks.left++
-    setClicks(clicks)
+    setAll(allClicks.concat('L')) // concat() makes a new array
+    // Do NOT use push() because it's a mutating method -> not good for React
+    setLeft(left + 1)
   }
-  */
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
 
   return (
     <div>
-      {clicks.left}
+      {left}
       <button onClick={handleLeftClick}>left</button>
       <button onClick={handleRightClick}>right</button>
-      {clicks.right}
+      {right}
+      <p>{allClicks.join(' ')}</p>
     </div>
   )
 }
 
 export default App
-
-/*
-Storing all of the state in a single state object is a bad choice for this particular application; there's no apparent benefit and the resulting application is a lot more complex. In this case storing the click counters into separate pieces of state is a far more suitable choice.
-*/
