f1bbb6ce0cf7881e7df8a4bd844f807839a81852
Sonja Ek <sonja3k@icloud.com>
2021-03-21 13:10:57 +0200

    Part 1c, Event handler is a function

--- tutorial-notes/part1/src/App.js
+++ tutorial-notes/part1/src/App.js
@@ -3,13 +3,20 @@ import React, { useState } from 'react' // useState is a function
 const App = () => {
   const [ counter, setCounter ] = useState(0)
 
+  // Event handler must be a function. If you write
+  // <button onClick={setCounter(counter + 1)}>, it's a *function call*
+  // and will not work at all.
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const setToZero = () => setCounter(0)
+
   return (
     <div>
       <div>{counter}</div>
-      <button onClick={() => setCounter(counter + 1)}>
+      <button onClick={increaseByOne}>
         plus
       </button>
-      <button onClick={() => setCounter(0)}>
+      <button onClick={setToZero}>
         zero
       </button>
     </div>
