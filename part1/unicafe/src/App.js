89bf94adb0f13ce3d251cd066d2d195c264f1f0a
Sonja Ek <sonja3k@icloud.com>
2021-03-22 10:21:10 +0200

    Unicafe, step 2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -10,16 +10,31 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+
+  // Not sure if this is a smart way to avoid NaN-values in the beginning:
+  let average;
+  let goodPercentage;
+  if (all !== 0) {
+    average = (good - bad) / all
+    goodPercentage = 100 * good / all
+  } else {
+    average = 0
+    goodPercentage = 0
+  }  
 
   const handleGood = () => {
+    setAll(all + 1)
     setGood(good + 1)
   }
 
   const handleNeutral = () => {
+    setAll(all + 1)
     setNeutral(neutral + 1)
   }
 
   const handleBad = () => {
+    setAll(all + 1)
     setBad(bad + 1)
   }
 
@@ -36,6 +51,9 @@ const App = () => {
         <p>good {good}</p>
         <p>neutral {neutral}</p>
         <p>bad {bad}</p>
+        <p>all {all}</p>
+        <p>average {average}</p>
+        <p>positive {goodPercentage} %</p>
       </div>
     </div>
   )
