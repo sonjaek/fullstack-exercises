1baef0efbeb98ad63c8ee5f397c865b848f2fa5a
Sonja Ek <sonja3k@icloud.com>
2021-03-22 15:19:31 +0200

    Anecdotes, step 3

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -18,23 +18,44 @@ const App = () => {
    
   const [selected, setSelected] = useState(0)
   const [points, setPoints] = useState(new Array(6).fill(0))
+  const [best, setBest] = useState(0)
 
   const handleVoteClick = () => {
     const copy = [...points]
     copy[selected] += 1
     setPoints(copy)
+    mostVoted()
   }
 
   const handleShuffleClick = () => {
     setSelected(Math.floor(Math.random() * 5 - 1) + 1)
   }
 
+  function mostVoted() {
+    let mostPopular = 0
+    let biggestValue = 0
+    for (let i = 0; i < points.length; i++) {
+      if (points[i] > biggestValue) {
+        mostPopular = i
+        biggestValue = points[i]
+      }
+    }
+    setBest(mostPopular)
+  }
+
   return (
     <div>
-      <p>{anecdotes[selected]}</p>
-      <p>has {points[selected]} points</p>
-      <Button handleClick={handleVoteClick} text='vote' />
-      <Button handleClick={handleShuffleClick} text='next anecdote' />
+      <div id='random-anecdotes'>
+        <h2>Anecdote of the day</h2>
+        <p>{anecdotes[selected]}</p>
+        <p>has {points[selected]} points</p>
+        <Button handleClick={handleVoteClick} text='vote' />
+        <Button handleClick={handleShuffleClick} text='next anecdote' />
+      </div>
+      <div id='best-anecdote'>
+        <h2>Anecdote with most votes</h2>
+        <p>{anecdotes[best]}</p>
+      </div>
     </div>
   )
 }
