dbabdcb756b06d50437823dc127c730aa2a422f2
Sonja Ek <sonja3k@icloud.com>
2021-04-01 15:07:09 +0300

    Add part 1 of Phonebook

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+
+// const Person = (person) => {
+//   console.log(person.name);
+//   return (
+//     <div>
+//       {person.name}
+//     </div>
+//   )
+// }
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      name: newName
+    }
+
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
+
+  const handleNameTyping = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input 
+            value={newName}
+            onChange={handleNameTyping}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => person.name)}
+      {/* {persons.map(person =>
+        <Person key={person.name} person={person} />
+      )} */}
+    </div>
+  )
+}
+
+export default App
