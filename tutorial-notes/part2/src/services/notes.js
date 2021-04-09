02e834aea1864bcd61209a30b4b96353f75437ab
Sonja Ek <sonja3k@icloud.com>
2021-04-09 18:11:43 +0300

    Part 2e: Add styling

--- tutorial-notes/part2/src/services/notes.js
+++ tutorial-notes/part2/src/services/notes.js
@@ -27,6 +27,5 @@ const update = (id, newObject) => {
   return request.then(response => response.data)
 }
 
-export default { 
-  getAll, create, update
-}
+const exportables = { getAll, create, update }
+export default exportables
