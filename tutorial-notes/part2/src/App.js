75497a695cd922ae0896f1f18bd105fe8e9b889a
Sonja Ek <sonja3k@icloud.com>
2021-04-09 18:15:40 +0300

    Part 2e, inline styles

--- tutorial-notes/part2/src/App.js
+++ tutorial-notes/part2/src/App.js
@@ -14,6 +14,20 @@ const Notification = ({ message }) => {
   )
 }
 
+const Footer = () => {
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italic',
+    fontSize: 16
+  }
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>Note app, Department of Computer Science, University of Helsinki 2021</em>
+    </div>
+  )
+}
+
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
@@ -98,6 +112,7 @@ const App = () => {
         />
         <button type="submit">save</button>
       </form>  
+      <Footer />
     </div>
   )
 }
