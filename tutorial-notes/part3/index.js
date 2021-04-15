74ceda04bad9cf2c4f025cabc322344ece3fc4c4
Sonja Ek <sonja3k@icloud.com>
2021-04-15 13:51:09 +0300

    Part 3a, Deleting resources

--- tutorial-notes/part3/index.js
+++ tutorial-notes/part3/index.js
@@ -40,6 +40,19 @@ app.get('/api/notes/:id', (request, response) => {
   }
 })
 
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+// If deleting the resource is successful, meaning that the note exists and it 
+// is removed, we respond to the request with the status code 204 no content 
+// and return no data with the response.
+
+// There's no consensus on what status code should be returned to a DELETE 
+// request if the resource does not exist.
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
