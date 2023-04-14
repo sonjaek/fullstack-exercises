be63782353dc3b2d0aa9ea9d0ce0e1f27f5cef26
Sonja Ek <sonja3k@icloud.com>
2023-04-14 10:51:14 +0300

    Allow adding new notes

--- part3/notebackend/index.js
+++ part3/notebackend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json()) // Use JSON parser
+
 let notes = [
   {
     id: 1,
@@ -43,6 +45,33 @@ app.delete('/api/notes/:id', (request, response) => {
   notes = notes.filter(note => note.id !== id)
 
   response.status(204).end()
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id)) // spread syntax: array -> individual numbers
+    : 0
+  return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
 })
 
 const PORT = 3001
